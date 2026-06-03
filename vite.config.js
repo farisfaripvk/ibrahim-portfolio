0cfe262740ab06b5967690c3add688339ec73eaa
Ibrahim Faris <farisfaripvk@gmail.com>
2026-06-03 20:03:00 +0400

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/ibrahim-portfolio/",
+  // base: "/ibrahim-portfolio/",
+  base: "./",
   plugins: [react()],
 });
